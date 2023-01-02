3f2b069a091083762b9617033d203fba69aca888
RohiniKatkar <rohinikatkar39@gmail.com>
2023-01-02 14:14:25 +0530

    first change

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require ("express")
+const app=express();
+const path=require("path")
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/',function(req,resp)
+{
+resp.sendFile(path.join(__dirname +"/index.html"));
+});
+app.listen(3000,function()
+{
+console.log("server is running on 3000")
+}
+)
+
+
+
